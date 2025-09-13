85bbb2979afec50a4230170387307bea4078127d
DIEGO RABELO <164809028+diegorabelorn@users.noreply.github.com>
2025-09-12 23:15:05 -0300

    Create creditos.js

---
+++ Geogando/creditos.js
@@ -0,0 +1,17 @@
+function creditos(){
+  image(home,435,10, 60, 60);
+  image(Creditos,130,90, 250, 250);
+  
+  stroke(color(0, 170, 255))
+  fill(51);
+  rect(100, 20, 300, 35, 35);
+  strokeWeight(2);
+  stroke(color(0, 170, 255));
+  c = color(255);
+  fill(c);
+  textSize(15)
+  textStyle(NORMAL);
+  text("CRÉDITOS", 215, 43);
+  
+  
+}
