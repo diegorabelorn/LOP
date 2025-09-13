5ff14a057389f60e730d1945d4c9752372921229
DIEGO RABELO <164809028+diegorabelorn@users.noreply.github.com>
2025-09-12 23:13:54 -0300

    Create opcoes.js

---
+++ Geogando/opcoes.js
@@ -0,0 +1,20 @@
+function opcoes(){
+  image(home,435,10, 60, 60);
+  
+  stroke(color(0, 170, 255))
+  fill(51);
+  rect(100, 20, 300, 35, 35);
+  strokeWeight(2);
+  stroke(color(0, 170, 255));
+  c = color(255);
+  fill(c);
+  textSize(15)
+  textStyle(NORMAL);
+  text("OPÇÕES", 215, 43);
+  
+  
+  
+  
+  
+  
+}
